183d9fc7fdd24fede9846a2571a869ad28029293
John Drago <drago@inflection.com>
2014-11-18 11:50:49 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hellothere::Application.config.secret_token = 'da61e947ef19323d69eecb9f7a0a84a71874b3f68213106a989834ddfc36ab56c1afc6e63a1aee2a4f41307b723e0600f9a9f6833b800c4a3d1fa974d2d5fea6'
